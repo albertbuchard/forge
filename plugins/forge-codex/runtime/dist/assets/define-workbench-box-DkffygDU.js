04bf0f91006ceb77ea7691f339c1688bc52c7465
Albert Buchard <albert.buchard@gmail.com>
2026-06-08 01:55:30 +0200

    release(openclaw): v0.3.4

---
+++ plugins/forge-codex/runtime/dist/assets/define-workbench-box-DkffygDU.js
@@ -0,0 +1,1 @@
+import{i as t}from"./generic-node-view-COhU_Wm3.js";function p(n,e){const o=r=>n(r);return o.displayName=e.title.replace(/\s+/g,""),t(o,e)}export{p as d};
--- plugins/forge-codex/runtime/dist/assets/open-in-graph-button-B_4fdm4l.js
+++
@@ -1,1 +0,0 @@
-import{b6 as p,j as s,aU as c}from"./vendor-Cpmju3nw.js";import{B as u,c7 as m}from"./index-wEZUnRh0.js";function d({entityType:a,entityId:e,view:n,label:o="Open in graph",variant:r="secondary",size:t}){const i=p();return s.jsxs(u,{variant:r,size:t,onClick:()=>i(m(a,e,{view:n})),children:[s.jsx(c,{className:"size-4"}),o]})}export{d as O};
